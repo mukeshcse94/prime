6d7de8456e00c96a84a0f2bdb3934bc022c246c4
mukesh <mukese0794@gmail.com>
2021-02-27 01:00:17 +0530

    clouser

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
-  state={
-    mockData:'["VALUE1", "VALUE2", "VALUE3", "VALUE4", "VALUE5"]'
-  };
+  handleCloser = (msg) => (e) => {
+    console.log(`call clousersssssssssssssssss ${msg}`, e);
+  }
   
   render() {
-    const parseValue = JSON.parse(this.state.mockData);
     return (
       <div>
-        {parseValue.map(items => {
-          return <li>{items}</li>
-        })}
+        <button onClick={this.handleCloser('called closer')}>Button</button>
       </div>
     )
   }
