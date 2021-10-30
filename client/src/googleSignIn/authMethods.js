21dd362295b04280838c478d9e2f3dcfd750b89d
mukeshcse94 <79327840+mukeshcse94@users.noreply.github.com>
2021-10-30 14:55:42 +0530

    cline initialize

--- client/src/googleSignIn/authMethods.js
+++
@@ -1,5 +0,0 @@
-import firebase from './firebase';
-
-export const facebookProvider = new firebase.auth.FacebookAuthProvider();
-export const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const githubProvider = new firebase.auth.GithubAuthProvider();
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DataProvider from './redux/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
